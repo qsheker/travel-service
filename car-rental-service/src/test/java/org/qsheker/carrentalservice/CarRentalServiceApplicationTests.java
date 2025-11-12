fc4a28f2a1ff93a0e2db2f2aefbd2863f69f5858
Aldiyar <aldikzhaks@gmail.com>
2025-11-12 17:27:03 +0500

    init commit

---
+++ car-rental-service/src/test/java/org/qsheker/carrentalservice/CarRentalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.qsheker.carrentalservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
